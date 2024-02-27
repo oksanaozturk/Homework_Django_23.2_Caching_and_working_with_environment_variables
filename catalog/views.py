9e59837a8c1e06b4b80795901f1b6fd1d77b8200
casper <oksanaozturk@gmail.com>
2024-02-27 23:17:04 +0300

    Добавлены контроллеры products_list и product_detail

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product
 
 
-def home_page(request):
-    return render(request, 'catalog/home_page.html')
+# def home_page(request):
+#     return render(request, 'catalog/home_page.html')
 
 
 def contacts(request):
@@ -14,3 +15,24 @@ def contacts(request):
         # а также передается информация, которую заполнил пользователь
         print(name, phone, message)
     return render(request, 'catalog/contacts.html')
+
+
+def products_list(request):
+    """Вывод страницу со всеми продуктами"""
+    products = Product.objects.all()
+    context = {
+        'object_list': products
+    }
+    return render(request, 'catalog/products_list.html', context)
+
+
+def product_detail(request, pk):
+    """Вывод страницы с одним продуктом по pk"""
+
+    # Возможно использовать get, но get_object_or_404 лучше
+    # product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': product
+    }
+    return render(request, 'catalog/product_detail.html', context)
