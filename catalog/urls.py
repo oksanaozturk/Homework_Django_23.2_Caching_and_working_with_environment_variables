21d0ca579cea5534aee54e50a86d5ed4ec55758e
casper <oksanaozturk@gmail.com>
2024-02-27 23:17:51 +0300

    Добавлены пути для product_detail, products_list

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from catalog.views import home_page, contacts
+from catalog.views import contacts, product_detail, products_list
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', home_page, name='home_page'),
-    path('contacts/', contacts, name='contacts')
+    path('', products_list, name='products_list'),
+    # path('', home_page, name='home_page'),
+    path('contacts/', contacts, name='contacts'),
+    path('product/<int:pk>/', product_detail, name='product_detail')
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
