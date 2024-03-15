c2990c07b76a8b1de0ba2865be8318c1b633e854
casper <oksanaozturk@gmail.com>
2024-03-15 21:32:40 +0300

    замена контроллера FBV на CBV. Созданы 3 новых класса ContactsTemplateView, ProductListView, ProductDetailView

--- catalog/views.py
+++ catalog/views.py
@@ -1,38 +1,51 @@
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, ListView, DetailView
+
 from catalog.models import Product
 
 
-# def home_page(request):
-#     return render(request, 'catalog/home_page.html')
+# замена контроллера FBV на CBV
+#  Новый контроллер CBV
+class ContactsTemplateView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+#  Старый контроллер FBV
+# def contacts(request):
+#     if request.method == 'POST':
+#         # в переменной request хранится информация о методе, который отправлял пользователь
+#         name = request.POST.get('name')
+#         phone = request.POST.get('phone')
+#         message = request.POST.get('message')
+#         # а также передается информация, которую заполнил пользователь
+#         print(name, phone, message)
+#     return render(request, 'catalog/contacts.html')
 
 
-def contacts(request):
-    if request.method == 'POST':
-        # в переменной request хранится информация о методе, который отправлял пользователь
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        # а также передается информация, которую заполнил пользователь
-        print(name, phone, message)
-    return render(request, 'catalog/contacts.html')
+#  Новый контроллер CBV
+class ProductListView(ListView):
+    """Класс для вывода страницы со всеми продуктами"""
+    model = Product
+
+#  Старый контроллер FBV
+# def products_list(request):
+#     """Вывод страницу со всеми продуктами"""
+#     products = Product.objects.all()
+#     context = {
+#         'object_list': products
+#     }
+#     return render(request, 'catalog/products_list.html', context)
 
 
-def products_list(request):
-    """Вывод страницу со всеми продуктами"""
-    products = Product.objects.all()
-    context = {
-        'object_list': products
-    }
-    return render(request, 'catalog/products_list.html', context)
+class ProductDetailView(DetailView):
+    """Класс для вывода страницы с одним продуктом по pk"""
+    model = Product
 
-
-def product_detail(request, pk):
-    """Вывод страницы с одним продуктом по pk"""
-
-    # Возможно использовать get, но get_object_or_404 лучше
-    # product = Product.objects.get(pk=pk)
-    product = get_object_or_404(Product, pk=pk)
-    context = {
-        'object': product
-    }
-    return render(request, 'catalog/product_detail.html', context)
+# def product_detail(request, pk):
+#     """Вывод страницы с одним продуктом по pk"""
+#
+#     # Возможно использовать get, но get_object_or_404 лучше
+#     # product = Product.objects.get(pk=pk)
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {
+#         'object': product
+#     }
+#     return render(request, 'catalog/product_detail.html', context)
