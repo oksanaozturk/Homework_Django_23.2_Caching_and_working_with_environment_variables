39b1d70d741044097e958be4d59bde1c76badd70
casper <oksanaozturk@gmail.com>
2024-03-15 21:40:14 +0300

    Добавлены названия классов

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,12 +4,13 @@ from catalog.models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    """Класс для регистрации категории в админке."""
     list_display = ('id', 'name',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    """ Класс для регистрации продукта в админке."""
     list_display = ('id', 'name', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description',)
-
