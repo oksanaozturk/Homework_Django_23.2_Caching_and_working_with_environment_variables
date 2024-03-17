a7ed199121866b70743ac593e03434650c975a6d
casper <oksanaozturk@gmail.com>
2024-03-17 22:20:59 +0300

    Добавление в класс для отображения Контактов метода post. Это дает возможность получать данные введенные Пользователем в консоли

--- catalog/views.py
+++ catalog/views.py
@@ -8,6 +8,13 @@ from catalog.models import Product
 class ContactsTemplateView(TemplateView):
     template_name = 'catalog/contacts.html'
 
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(name, phone, message)
+        return super().get(request, *args, **kwargs)
+
 #  Старый контроллер FBV
 # def contacts(request):
 #     if request.method == 'POST':
