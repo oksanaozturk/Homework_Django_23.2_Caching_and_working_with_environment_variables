261160d5f295697d7f0928104f3dde215f6904c2
casper <oksanaozturk@gmail.com>
2024-03-17 08:01:34 +0300

    Добавлена prepopulated_fields для автоматического формирования slug при создании новой публикации через Админку

--- blog/admin.py
+++ blog/admin.py
@@ -9,3 +9,4 @@ class BlogAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'slug', 'date_of_creation', 'is_published', 'view_counter')
     list_filter = ('date_of_creation',)
     search_fields = ('title', 'is_published',)
+    prepopulated_fields = {"slug": ("title",)}
