563542d2a06b49032c1099a4dd387591b49873c8
casper <oksanaozturk@gmail.com>
2024-03-17 08:02:39 +0300

    Замена отображения d genz[ экземпляров c рк на отображение по slug

--- blog/urls.py
+++ blog/urls.py
@@ -11,8 +11,8 @@ app_name = BlogConfig.name
 urlpatterns = [
     path('create/', BlogCreateView.as_view(), name='create'),
     path('', BlogListView.as_view(), name='list'),
-    path('update/<int:pk>', BlogUpdateView.as_view(), name='update'),
-    path('view/<int:pk>', BlogDetailView.as_view(), name='view'),
-    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete')
+    path('update/<slug:slug>', BlogUpdateView.as_view(), name='update'),
+    path('view/<slug:slug>', BlogDetailView.as_view(), name='view'),
+    path('delete/<slug:slug>/', BlogDeleteView.as_view(), name='delete')
 
 ]
