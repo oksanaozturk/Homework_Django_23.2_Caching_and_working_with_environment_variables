3780072727834d1dc5b49a9da1556a74ba8b2e59
casper <oksanaozturk@gmail.com>
2024-03-25 00:47:41 +0300

    Добавлена модель Version

--- catalog/models.py
+++ catalog/models.py
@@ -7,6 +7,7 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class Category(models.Model):
+    """Модель для Категорий продукта"""
 
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание', **NULLABLE)
@@ -21,6 +22,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    """Модель для продукта"""
 
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание', **NULLABLE)  # blank=True. null=True
@@ -33,6 +35,10 @@ class Product(models.Model):
     date_of_creation = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     last_modified_date = models.DateField(auto_now=True, verbose_name='Дата изменения')
 
+    view_counter = models.PositiveIntegerField(verbose_name='Счетчик просмотров',
+                                               help_text="Укажите количество просмотров",
+                                               default=0)
+
     # manufactured_at = models.DateField(auto_now=True, verbose_name='Дата производства', **NULLABEL)
 
     # Необходимо для отображения модели на русскорм языке в административной панели
@@ -40,6 +46,24 @@ class Product(models.Model):
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
         ordering = ('name',)  # сортировка по данному параметру. В кортежах и списках ставим запятую в конце!!!
+
+    def __str__(self):
+        return self.name
+
+
+class Version(models.Model):
+    """Модель для версии продукта"""
+    # related_name говорит об отношении один ко многим (у одного продукта м.б. несколько версий).
+    # М.Б. обращаться как product.versions, а не product.version_set
+    product = models.ForeignKey(Product, related_name='versions', verbose_name='Продукт', on_delete=models.CASCADE)
+    number = models.PositiveIntegerField(verbose_name=' Номер версии', blank=True, null=True)
+    name = models.CharField(max_length=150, verbose_name='Название версии')
+    is_current = models.BooleanField(default=True, verbose_name='Признак актуальности')
+
+    class Meta:
+        verbose_name = 'Версия'
+        verbose_name_plural = 'Версии'
+        ordering = ('name',)
 
     def __str__(self):
         return self.name
