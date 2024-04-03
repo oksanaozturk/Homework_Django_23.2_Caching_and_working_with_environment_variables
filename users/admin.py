9bf5fa0640961b95d599a67fac946dfc0c7c771c
casper <oksanaozturk@gmail.com>
2024-04-03 23:12:45 +0300

    Настройка отображения Пользователя в Админке

---
+++ users/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Класс для регистрации Пользователя в админке."""
+
+    list_display = ("id", "email", "is_active", "password", "avatar", "phone_number")
+    list_filter = ("email",)
+    search_fields = ("email",)
